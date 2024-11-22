40c8bfd23721fb35652db4188c7236dcbf7b588f
Amr El-Qattan <amrelqattan95@gmail.com>
2024-11-22 21:11:25 +0200

    edit fetch

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,31 @@
-import Image from "next/image";
 import Results from "./components/ResultsApi/Results";
+
 const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
-  const genre = searchParams.genre || "fetchTrending";
-  const res = await fetch(
-    `https://api.themoviedb.org/3${
-      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`,
-    {next: {revalidate:60}},
-    // { cache: "no-cache" }
-  );
-  const data = await res.json();
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  const results = data.results;
-  // console.log(results);
+  const genre = (await searchParams)?.genre || "fetchTrending"; 
 
-  return (
-    <>
-      <Results results={results} />
-    </>
-  );
+  try {
+    const res = await fetch(
+      `https://api.themoviedb.org/3${
+        genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+      }?api_key=${API_KEY}&language=en-US&page=1`,
+      { next: { revalidate: 60 } }
+    );
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch data: ${res.statusText}`);
+    }
+
+    const data = await res.json();
+
+    return (
+      <div>
+        <Results results={data.results} />
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return <h1 className="text-center mt-5">Error: {error.message}</h1>;
+  }
 }
